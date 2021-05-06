75a3e99850e522581bd47cb1097e63b8f1ea1bb6
Matias <matiaspagnuolo@gmail.com>
2021-05-06 17:21:44 -0300

    cambos asignacion

--- calcular.js
+++ calcular.js
@@ -5,7 +5,7 @@ var fin = document.getElementById("imprimir");
 
 
 //sintaxis de una funcion
-function inicio(){
+function inicio() {
     numero_1 = prompt("Ingrese un numero: ");// el prompt tomo lo ingresado como texto, hay que pasarlo a numero
     numero_2 = prompt("Ingrese otro numero: ");
 
@@ -14,7 +14,7 @@ function inicio(){
     numero_2 = parseFloat(numero_2);
 }
 
-function sumar(){
+function sumar() {
     // sintaxis del if/else
 
     if(!numero_1 || !numero_2){ 
@@ -22,40 +22,40 @@ function sumar(){
         inicio()
     }else{
         resultado = numero_1 + numero_2;
-        fin.innerHTML = (resultado)
-    } 
+        fin.innerHTML = resultado;
+        } 
     
     }
 
-function restar(){
+function restar() {
      if(!numero_1 || !numero_2){
         alert("Por favor, ingrese un numero");
         inicio()
      }else{
          resultado = numero_1 - numero_2;
-         fin.innerHTML(resultado);
+         fin.innerHTML = resultado;
 
          //console.log("el resultado de la resta de" + numero_1 - "-" - numero_2 + "es: " + restar);
         }
       }   
 
-function multiplicar(){
+function multiplicar() {
     if(!numero_1 || !numero_2){
         alert("Por favor, ingrese un numero");
         inicio()
      }else{
          resultado = numero_1 * numero_2;
-         fin.innerHTML(resultado);
+         fin.innerHTML = resultado;
          //console.log("el resultado de la multiplicacion de" + numero_1 * "*" * numero_2 + "es: " + multiplicar);
         }
     }
-function dividir(){
+function dividir() {
     if(!numero_1 || !numero_2){
         alert("Por favor, ingrese un numero");
         inicio()
      }else{
          resultado = numero_1 / numero_2;
-         fin.innerHTML(resultado); 
+         fin.innerHTML = resultado; 
          //console.log("el resultado de la division de" + numero_1 / "/" / numero_2 + "es: " + dividir);
         }
 }    
